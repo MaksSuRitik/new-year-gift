9dcf38d6c92f898d39ebce60675d23b59524a949
MaksSuRitik <makssuharev2006@gmail.com@>
2025-12-20 18:30:57 +0200

    Напишите здесь описание изменений

--- app/js/script.js
+++ app/js/script.js
@@ -283,7 +283,7 @@ if (spinBtn) {
         { title: "Если гора не идёт к Магомеду, то Магомед спускается с горы", file: "magomed.mp4", rarity: "epic" },
         { title: "Похвасталась", file: "compat.mp4", rarity: "epic" },
         { title: "Иногда для счастья надо нырнуть щучкой", file: "dive.mp4", rarity: "rare" },
-        { title: "Ну всё так всё", file: "all.mp4", rarity: "common" },
+        { title: "Ротик шире (улыбка , а вы о чем)", file: "all.mp4", rarity: "common" },
         { title: "Только в момент тишины осознаёшь ценность звука", file: "sound.mp4", rarity: "common" }
     ];
 
